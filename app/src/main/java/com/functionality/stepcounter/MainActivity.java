51eb5a74e7c0d479154c62e1e81b0700d8917006
tranleminh <tranleminh.ams@gmail.com>
2019-06-20 22:02:28 +0200

    Initial commit

---
+++ app/src/main/java/com/functionality/stepcounter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.functionality.stepcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
